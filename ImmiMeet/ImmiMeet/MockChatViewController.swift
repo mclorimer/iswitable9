c652e37a9294041ca3ab1897e3a56810637927b5
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-07-15 11:58:32 -0400

    added mock chat

---
+++ ImmiMeet/ImmiMeet/MockChatViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MockChatViewController.swift
+//  ImmiMeet
+//
+//  Created by Marty Avedon on 7/15/17.
+//  Copyright © 2017 ImmiMeet118118. All rights reserved.
+//
+
+import UIKit
+
+class MockChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

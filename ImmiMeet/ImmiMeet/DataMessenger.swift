b5d7480260d20a3a156c204b0c6c64719fe6c5bf
Liam <liamdkane@gmail.com>
2017-07-15 05:57:55 -0400

    datamessenger

---
+++ ImmiMeet/ImmiMeet/DataMessenger.swift
@@ -0,0 +1,16 @@
+//
+//  DataMessenger.swift
+//  ImmiMeet
+//
+//  Created by C4Q on 7/15/17.
+//  Copyright © 2017 ImmiMeet118118. All rights reserved.
+//
+
+import Foundation
+
+struct DataMessenger {
+    private static let apiManager = APIRequestManager.manager
+    
+    static func getUsers () {
+    }
+}
